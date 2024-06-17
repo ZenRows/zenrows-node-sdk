6680f0a03d349027849076f6d6b974551bd1ab4c
Néstor <nestor@nstlopez.com>
2024-06-17 12:02:37 +0200

    feat: format file to compliment with new configs

--- examples/index.js
+++ examples/index.js
@@ -1,49 +1,50 @@
 /* eslint-disable @typescript-eslint/no-var-requires, no-console */
-const { ZenRows } = require('zenrows');
+const { ZenRows } = require("zenrows");
 
-const apiKey = 'YOUR-API-KEY';
-const urlLinks = 'https://www.zenrows.com/';
-const urlPremium = 'https://www.google.com/search?q=Ariana+Grande';
-const testPost = 'https://httpbin.org/anything';
+const apiKey = "YOUR-API-KEY";
+const urlLinks = "https://www.zenrows.com/";
+const urlPremium = "https://www.google.com/search?q=Ariana+Grande";
+const testPost = "https://httpbin.org/anything";
 
 (async () => {
-    const client = new ZenRows(apiKey, { concurrency: 5, retries: 1 });
+	const client = new ZenRows(apiKey, { concurrency: 5, retries: 1 });
 
-    try {
-        const { data } = await client.get(urlLinks, {
-            // autoparse: true,
-            css_extractor: '{"links": "a @href"}',
-            // js_render: true,
-            // premium_proxy: true,
-            // proxy_country: 'us',
-        });
+	try {
+		const { data } = await client.get(urlLinks, {
+			// autoparse: true,
+			css_extractor: '{"links": "a @href"}',
+			// js_render: true,
+			// premium_proxy: true,
+			// proxy_country: 'us',
+		});
 
-        console.log(data.links);
-        /*
+		console.log(data.links);
+		/*
             [
                 'https://www.zenrows.com',
                 'https://www.zenrows.com/blog',
                 ...
             ]
         */
-    } catch (error) {
-        console.error(error.message);
-        if (error.response) {
-            console.error(error.response.data);
-        }
-    }
+	} catch (error) {
+		console.error(error.message);
+		if (error.response) {
+			console.error(error.response.data);
+		}
+	}
 
-    try {
-        const { data } = await client.get(urlPremium, {
-            // autoparse: true,
-            css_extractor: '{"aproximate_results": "#result-stats", "headers": "#search a > h3"}',
-            // js_render: true,
-            premium_proxy: true,
-            proxy_country: 'us',
-        });
+	try {
+		const { data } = await client.get(urlPremium, {
+			// autoparse: true,
+			css_extractor:
+				'{"aproximate_results": "#result-stats", "headers": "#search a > h3"}',
+			// js_render: true,
+			premium_proxy: true,
+			proxy_country: "us",
+		});
 
-        console.log(data);
-        /*
+		console.log(data);
+		/*
             {
                 aproximate_results: 'About 10,700,000 results (0.48 seconds)',
                 headers: [
@@ -54,54 +55,58 @@ const testPost = 'https://httpbin.org/anything';
                 ]
             }
         */
-    } catch (error) {
-        console.error(error.message);
-        if (error.response) {
-            console.error(error.response.data);
-        }
-    }
+	} catch (error) {
+		console.error(error.message);
+		if (error.response) {
+			console.error(error.response.data);
+		}
+	}
 
-    try {
-        const urls = [
-            'https://api.ipify.org',
-            'https://httpbin.org/ip',
-            'https://httpbin.org/anything',
-            'https://ident.me',
-        ];
+	try {
+		const urls = [
+			"https://api.ipify.org",
+			"https://httpbin.org/ip",
+			"https://httpbin.org/anything",
+			"https://ident.me",
+		];
 
-        const promises = urls.map((url) => client.get(url));
+		const promises = urls.map((url) => client.get(url));
 
-        const results = await Promise.allSettled(promises);
-        const rejected = results.filter(({ status }) => status === 'rejected');
-        const fulfilled = results.filter(({ status }) => status === 'fulfilled');
+		const results = await Promise.allSettled(promises);
+		const rejected = results.filter(({ status }) => status === "rejected");
+		const fulfilled = results.filter(({ status }) => status === "fulfilled");
 
-        console.log(rejected);
-        console.log(fulfilled);
-    } catch (error) {
-        console.error(error.message);
-        if (error.response) {
-            console.error(error.response.data);
-        }
-    }
+		console.log(rejected);
+		console.log(fulfilled);
+	} catch (error) {
+		console.error(error.message);
+		if (error.response) {
+			console.error(error.response.data);
+		}
+	}
 
-    try {
-        const { data } = await client.post(testPost, {}, {
-            data: new URLSearchParams({
-                "key1": "value1",
-                "key2": "value2",
-            }).toString(),
-        });
+	try {
+		const { data } = await client.post(
+			testPost,
+			{},
+			{
+				data: new URLSearchParams({
+					key1: "value1",
+					key2: "value2",
+				}).toString(),
+			},
+		);
 
-        console.log(data);
-        /*
+		console.log(data);
+		/*
             ...
             form: { key1: 'value1', key2: 'value2' },
             ...
         */
-    } catch (error) {
-        console.error(error.message);
-        if (error.response) {
-            console.error(error.response.data);
-        }
-    }
+	} catch (error) {
+		console.error(error.message);
+		if (error.response) {
+			console.error(error.response.data);
+		}
+	}
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,129 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import axios, { AxiosPromise, AxiosRequestConfig, Method } from 'axios';
-import axiosRetry from 'axios-retry';
-import * as fastq from 'fastq';
+import axios, {
+	type AxiosPromise,
+	type AxiosRequestConfig,
+	type Method,
+} from "axios";
+import axiosRetry from "axios-retry";
+import * as fastq from "fastq";
 
-import { VERSION } from './version';
+import { VERSION } from "./version";
 
-const API_URL = 'https://api.zenrows.com/v1/';
+const API_URL = "https://api.zenrows.com/v1/";
 
 interface ClientConfig {
-    concurrency?: number;
-    retries?: number;
+	concurrency?: number;
+	retries?: number;
 }
 interface Config {
-    autoparse?: boolean;
-    /* eslint-disable camelcase */
-    css_extractor?: string;
-    js_render?: boolean;
-    premium_proxy?: boolean;
-    proxy_country?: string;
-    wait_for?: string;
-    wait?: number;
-    block_resources?: string;
-    window_width?: number;
-    window_height?: number;
-    device?: string;
-    original_status?: boolean;
-    custom_headers?: boolean;
-    /* eslint-enable camelcase */
-    [x: string]: unknown;
+	autoparse?: boolean;
+	css_extractor?: string;
+	js_render?: boolean;
+	premium_proxy?: boolean;
+	proxy_country?: string;
+	wait_for?: string;
+	wait?: number;
+	block_resources?: string;
+	window_width?: number;
+	window_height?: number;
+	device?: string;
+	original_status?: boolean;
+	custom_headers?: boolean;
+	[x: string]: unknown;
 }
 
 interface Headers {
-    [x: string]: string;
+	[x: string]: string;
 }
 
 class ZenRows {
-    readonly apiKey: string;
+	readonly apiKey: string;
+	readonly clientConfig: ClientConfig;
+	readonly queue;
 
-    readonly clientConfig: ClientConfig;
+	constructor(apiKey: string, clientConfig: ClientConfig = {}) {
+		this.apiKey = apiKey;
+		this.clientConfig = clientConfig;
 
-    readonly queue;
+		this.queue = fastq.promise(
+			this,
+			this.worker,
+			this.clientConfig.concurrency ?? 5,
+		);
 
-    constructor(apiKey: string, clientConfig: ClientConfig = {}) {
-        this.apiKey = apiKey;
-        this.clientConfig = clientConfig;
+		this.applyRetries();
+	}
 
-        this.queue = fastq.promise(this, this.worker, this.clientConfig.concurrency ?? 5);
+	public get(
+		url: string,
+		config?: Config,
+		{ headers = {} }: { headers?: Headers } = {},
+	): AxiosPromise {
+		return this.queue.push({ url, config, headers });
+	}
 
-        this.applyRetries();
-    }
+	public post(
+		url: string,
+		config?: Config,
+		{ headers = {}, data = {} }: { headers?: Headers; data?: unknown } = {},
+	): AxiosPromise {
+		return this.queue.push({ url, method: "POST", config, headers, data });
+	}
 
-    public get(url: string, config?: Config, { headers = {} }: { headers?: Headers } = {}): AxiosPromise {
-        return this.queue.push({ url, config, headers });
-    }
+	private worker({
+		url,
+		method = "GET",
+		config,
+		headers,
+		data,
+	}: {
+		url: string;
+		method?: Method;
+		config?: Config;
+		headers: Headers;
+		data?: unknown;
+	}): AxiosPromise {
+		const params = {
+			...config,
+			url,
+			apikey: this.apiKey,
+		};
 
-    public post(url: string, config?: Config, { headers = {}, data = {} }: { headers?: Headers, data?: any } = {}): AxiosPromise {
-        return this.queue.push({ url, method: 'POST', config, headers, data });
-    }
+		const finalHeaders = {
+			"User-Agent": `zenrows/${VERSION} node`,
+			...headers,
+		};
 
-    private worker({ url, method = 'GET', config, headers, data }: { url: string; method?: Method; config?: Config; headers: Headers, data?: any }): AxiosPromise {
-        const params = {
-            ...config,
-            url,
-            apikey: this.apiKey,
-        };
+		const axiosRequestConfig: AxiosRequestConfig = {
+			baseURL: API_URL,
+			method,
+			params,
+			headers: finalHeaders,
+			data,
+		};
 
-        const finalHeaders = {
-            'User-Agent': `zenrows/${VERSION} node`,
-            ...headers,
-        };
+		if (headers && Object.keys(headers).length) {
+			params.custom_headers = true;
+		}
 
-        const axiosRequestConfig: AxiosRequestConfig = {
-            baseURL: API_URL,
-            method,
-            params,
-            headers: finalHeaders,
-            data,
-        };
+		return axios(axiosRequestConfig);
+	}
 
-        if (headers && Object.keys(headers).length) {
-            params.custom_headers = true;
-        }
+	private applyRetries() {
+		const retries = this.clientConfig.retries ?? 0;
+		if (retries > 0) {
+			axiosRetry(axios, {
+				retries,
+				retryDelay: axiosRetry.exponentialDelay,
+				retryCondition: (error) => {
+					if (error.response?.status === 429) {
+						return true;
+					}
 
-        return axios(axiosRequestConfig);
-    }
-
-    private applyRetries() {
-        const retries = this.clientConfig.retries ?? 0;
-        if (retries > 0) {
-            axiosRetry(axios, {
-                retries,
-                retryDelay: axiosRetry.exponentialDelay,
-                retryCondition: (error) => {
-                    if (error.response?.status === 429) {
-                        return true;
-                    }
-
-                    return axiosRetry.isNetworkOrIdempotentRequestError(error);
-                },
-            });
-        }
-    }
+					return axiosRetry.isNetworkOrIdempotentRequestError(error);
+				},
+			});
+		}
+	}
 }
 
 export { ZenRows };
