da6e7b96989a1b2ffc57d22b841fa0983e1ddc83
Néstor <nestor@nstlopez.com>
2024-06-17 14:31:00 +0200

    chore: format test files

--- tests/concurrency.test.ts
+++ tests/concurrency.test.ts
@@ -1,24 +1,24 @@
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
-import { ZenRows } from '../src';
+import { ZenRows } from "../src";
 
-describe('ZenRows Client with Concurrency', () => {
-    const apiKey = 'API_KEY';
-    const url = 'https://zenrows.com';
-    const client = new ZenRows(apiKey, { concurrency: 2 });
+describe("ZenRows Client with Concurrency", () => {
+	const apiKey = "API_KEY";
+	const url = "https://zenrows.com";
+	const client = new ZenRows(apiKey, { concurrency: 2 });
 
-    test('should send and wait for two successful requests', async () => {
-        const mock = new MockAdapter(axios);
-        mock.onGet().reply(200);
+	test("should send and wait for two successful requests", async () => {
+		const mock = new MockAdapter(axios);
+		mock.onGet().reply(200);
 
-        const promises = [client.get(url), client.get(url)];
+		const promises = [client.get(url), client.get(url)];
 
-        const responses = await Promise.all(promises);
-        const [response1, response2] = responses;
+		const responses = await Promise.all(promises);
+		const [response1, response2] = responses;
 
-        expect(responses.length).toBe(2);
-        expect(response1.status).toBe(200);
-        expect(response2.status).toBe(200);
-    });
+		expect(responses.length).toBe(2);
+		expect(response1.status).toBe(200);
+		expect(response2.status).toBe(200);
+	});
 });
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,68 +1,70 @@
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
-import { ZenRows } from '../src';
+import { ZenRows } from "../src";
 
 const mock = new MockAdapter(axios);
 
-describe('ZenRows Client Get', () => {
-    const apiKey = 'API_KEY';
-    const url = 'https://zenrows.com';
-    const client = new ZenRows(apiKey);
-    mock.onGet().reply(200);
-    mock.onPost().reply(200);
+describe("ZenRows Client Get", () => {
+	const apiKey = "API_KEY";
+	const url = "https://zenrows.com";
+	const client = new ZenRows(apiKey);
+	mock.onGet().reply(200);
+	mock.onPost().reply(200);
 
-    test('should check response status and add custom user agent', async () => {
-        const response = await client.get(url);
+	test("should check response status and add custom user agent", async () => {
+		const response = await client.get(url);
 
-        expect(response.status).toBe(200);
-        expect(response.config.headers['User-Agent']).toMatch(/zenrows\/(.*) node/);
-    });
+		expect(response.status).toBe(200);
+		expect(response.config.headers["User-Agent"]).toMatch(/zenrows\/(.*) node/);
+	});
 
-    test('should set mandatory params api key and url correctly', async () => {
-        const response = await client.get(url);
+	test("should set mandatory params api key and url correctly", async () => {
+		const response = await client.get(url);
 
-        expect(response.config.params.apikey).toBe(apiKey);
-        expect(response.config.params.url).toBe(url);
-    });
+		expect(response.config.params.apikey).toBe(apiKey);
+		expect(response.config.params.url).toBe(url);
+	});
 
-    test('should set optional params correctly', async () => {
-        const customCssExtractor = '{"links": "a @href", "images": "img @src"}';
-        const response = await client.get(url, {
-            autoparse: true,
-            css_extractor: customCssExtractor,
-            js_render: true,
-            premium_proxy: true,
-            proxy_country: 'us',
-        });
+	test("should set optional params correctly", async () => {
+		const customCssExtractor = '{"links": "a @href", "images": "img @src"}';
+		const response = await client.get(url, {
+			autoparse: true,
+			css_extractor: customCssExtractor,
+			js_render: true,
+			premium_proxy: true,
+			proxy_country: "us",
+		});
 
-        expect(response.config.params.autoparse).toBe(true);
-        expect(response.config.params.css_extractor).toBe(customCssExtractor);
-        expect(response.config.params.js_render).toBe(true);
-        expect(response.config.params.premium_proxy).toBe(true);
-        expect(response.config.params.proxy_country).toBe('us');
-    });
+		expect(response.config.params.autoparse).toBe(true);
+		expect(response.config.params.css_extractor).toBe(customCssExtractor);
+		expect(response.config.params.js_render).toBe(true);
+		expect(response.config.params.premium_proxy).toBe(true);
+		expect(response.config.params.proxy_country).toBe("us");
+	});
 
-    test('should overwrite user agent from parameters', async () => {
-        const headers = { 'User-Agent': 'test' };
-        const response = await client.get(url, {}, { headers });
+	test("should overwrite user agent from parameters", async () => {
+		const headers = { "User-Agent": "test" };
+		const response = await client.get(url, {}, { headers });
 
-        expect(response.config.headers['User-Agent']).toBe('test');
-        expect(response.config.params.custom_headers).toBe(true);
-    });
+		expect(response.config.headers["User-Agent"]).toBe("test");
+		expect(response.config.params.custom_headers).toBe(true);
+	});
 
-    test('should check response status on POST request', async () => {
-        const response = await client.post(url);
+	test("should check response status on POST request", async () => {
+		const response = await client.post(url);
 
-        expect(response.status).toBe(200);
-    });
+		expect(response.status).toBe(200);
+	});
 
-    test('should check data on POST request', async () => {
-        const data = 'key1=value1&key2=value2';
-        const response = await client.post(url, {}, { data });
+	test("should check data on POST request", async () => {
+		const data = "key1=value1&key2=value2";
+		const response = await client.post(url, {}, { data });
 
-        expect(response.config.method).toBe('post');
-        expect(response.config.data).toBe(data);
-        expect(response.config.headers['Content-Type']).toBe('application/x-www-form-urlencoded');
-    });
+		expect(response.config.method).toBe("post");
+		expect(response.config.data).toBe(data);
+		expect(response.config.headers["Content-Type"]).toBe(
+			"application/x-www-form-urlencoded",
+		);
+	});
 });
