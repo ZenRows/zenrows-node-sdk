920d47cc29c2e2cd61f3d964f3b3f70258688f0e
Néstor <nestor@nstlopez.com>
2024-06-21 10:55:18 +0200

    chore: properly format with biome

--- src/index.ts
+++ src/index.ts
@@ -7,136 +7,136 @@ const API_URL = "https://api.zenrows.com/v1/";
 type HttpMethods = "GET" | "POST" | "PUT";
 
 interface ClientConfig {
-	concurrency?: number;
-	retries?: number;
+  concurrency?: number;
+  retries?: number;
 }
 export interface ZenrowsConfig {
-	autoparse?: boolean;
-	css_extractor?: string;
-	js_render?: boolean;
-	premium_proxy?: boolean;
-	proxy_country?: string;
-	wait_for?: string;
-	wait?: number;
-	block_resources?: string;
-	window_width?: number;
-	window_height?: number;
-	device?: string;
-	original_status?: boolean;
-	custom_headers?: boolean;
-	[x: string]: unknown;
+  autoparse?: boolean;
+  css_extractor?: string;
+  js_render?: boolean;
+  premium_proxy?: boolean;
+  proxy_country?: string;
+  wait_for?: string;
+  wait?: number;
+  block_resources?: string;
+  window_width?: number;
+  window_height?: number;
+  device?: string;
+  original_status?: boolean;
+  custom_headers?: boolean;
+  [x: string]: unknown;
 }
 
 interface Headers {
-	[x: string]: string;
+  [x: string]: string;
 }
 
 export class ZenRows {
-	readonly apiKey: string;
-	readonly clientConfig: ClientConfig;
-	readonly queue;
-	readonly fetchWithRetry;
+  readonly apiKey: string;
+  readonly clientConfig: ClientConfig;
+  readonly queue;
+  readonly fetchWithRetry;
 
-	constructor(apiKey: string, clientConfig: ClientConfig = {}) {
-		this.apiKey = apiKey;
-		this.clientConfig = clientConfig;
-		const retries = this.clientConfig.retries ?? 0;
+  constructor(apiKey: string, clientConfig: ClientConfig = {}) {
+    this.apiKey = apiKey;
+    this.clientConfig = clientConfig;
+    const retries = this.clientConfig.retries ?? 0;
 
-		this.queue = fastq.promise(
-			this,
-			this.worker,
-			this.clientConfig.concurrency ?? 5,
-		);
+    this.queue = fastq.promise(
+      this,
+      this.worker,
+      this.clientConfig.concurrency ?? 5,
+    );
 
-		this.fetchWithRetry = fetchRetry(fetch, {
-			retryDelay: (attempt) => 2 ** attempt * 1000,
-			// retryOn: [422, 503, 504],
-			retryOn: (attempt, error, response) => {
-				if (attempt > retries) {
-					return false;
-				}
+    this.fetchWithRetry = fetchRetry(fetch, {
+      retryDelay: (attempt) => 2 ** attempt * 1000,
+      // retryOn: [422, 503, 504],
+      retryOn: (attempt, error, response) => {
+        if (attempt > retries) {
+          return false;
+        }
 
-				if (
-					error !== null ||
-					response?.status === 422 ||
-					response?.status === 503 ||
-					response?.status === 504
-				) {
-					return true;
-				}
+        if (
+          error !== null ||
+          response?.status === 422 ||
+          response?.status === 503 ||
+          response?.status === 504
+        ) {
+          return true;
+        }
 
-				return false;
-			},
-		});
-	}
+        return false;
+      },
+    });
+  }
 
-	public get(
-		url: string,
-		config?: ZenrowsConfig,
-		{ headers = {} }: { headers?: Headers } = {},
-	): Promise<Response> {
-		return this.queue.push({ url, config, headers });
-	}
+  public get(
+    url: string,
+    config?: ZenrowsConfig,
+    { headers = {} }: { headers?: Headers } = {},
+  ): Promise<Response> {
+    return this.queue.push({ url, config, headers });
+  }
 
-	public post(
-		url: string,
-		config?: ZenrowsConfig,
-		{ headers = {}, data = {} }: { headers?: Headers; data?: unknown } = {
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-		},
-	): Promise<Response> {
-		return this.queue.push({ url, method: "POST", config, headers, data });
-	}
+  public post(
+    url: string,
+    config?: ZenrowsConfig,
+    { headers = {}, data = {} }: { headers?: Headers; data?: unknown } = {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    },
+  ): Promise<Response> {
+    return this.queue.push({ url, method: "POST", config, headers, data });
+  }
 
-	private async worker({
-		url,
-		method = "GET",
-		config,
-		headers,
-		data,
-	}: {
-		url: string;
-		method?: HttpMethods;
-		config?: ZenrowsConfig;
-		headers: Headers;
-		data?: unknown;
-	}): Promise<Response> {
-		const params = new URLSearchParams({
-			url,
-			apikey: this.apiKey,
-		});
+  private async worker({
+    url,
+    method = "GET",
+    config,
+    headers,
+    data,
+  }: {
+    url: string;
+    method?: HttpMethods;
+    config?: ZenrowsConfig;
+    headers: Headers;
+    data?: unknown;
+  }): Promise<Response> {
+    const params = new URLSearchParams({
+      url,
+      apikey: this.apiKey,
+    });
 
-		if (config) {
-			for (const [key, value] of Object.entries(config)) {
-				if (value !== undefined) {
-					params.append(key, String(value));
-				}
-			}
-		}
+    if (config) {
+      for (const [key, value] of Object.entries(config)) {
+        if (value !== undefined) {
+          params.append(key, String(value));
+        }
+      }
+    }
 
-		if (headers && Object.keys(headers).length) {
-			params.append("custom_headers", "true");
-		}
+    if (headers && Object.keys(headers).length) {
+      params.append("custom_headers", "true");
+    }
 
-		const finalHeaders = {
-			"User-Agent": `zenrows/${packageJson.version} node`,
-			...headers,
-		};
+    const finalHeaders = {
+      "User-Agent": `zenrows/${packageJson.version} node`,
+      ...headers,
+    };
 
-		const fetchOptions: RequestInit = {
-			method,
-			headers: finalHeaders,
-		};
+    const fetchOptions: RequestInit = {
+      method,
+      headers: finalHeaders,
+    };
 
-		if (method === "POST" && data) {
-			fetchOptions.body = JSON.stringify(data);
-		}
+    if (method === "POST" && data) {
+      fetchOptions.body = JSON.stringify(data);
+    }
 
-		const response = await this.fetchWithRetry(
-			`${API_URL}?${params.toString()}`,
-			fetchOptions,
-		);
+    const response = await this.fetchWithRetry(
+      `${API_URL}?${params.toString()}`,
+      fetchOptions,
+    );
 
-		return response;
-	}
+    return response;
+  }
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {
-		globals: true,
-		include: ["tests/**/*.test.ts"],
-		setupFiles: ["tests/_setup.ts"],
-	},
+  test: {
+    globals: true,
+    include: ["tests/**/*.test.ts"],
+    setupFiles: ["tests/_setup.ts"],
+  },
 });
